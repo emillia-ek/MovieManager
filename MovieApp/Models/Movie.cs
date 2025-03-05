89452e608d9e6547a7ddfaf1bb629d3e606b944f
Emilia <emikoscielniak@gmail.com>
2025-03-05 20:40:49 +0100

    Add

---
+++ MovieApp/Models/Movie.cs
@@ -0,0 +1,10 @@
+﻿namespace MovieApp.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public required string Title { get; set; }
+        public required string Director { get; set; }
+        public int Year { get; set; }
+    }
+}
