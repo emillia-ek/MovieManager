b859a16fc5510465f001329adbc73729668d9aef
Emilia <emikoscielniak@gmail.com>
2025-03-09 21:08:37 +0100

    delete

--- MovieApp/Models/AddMovieDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MovieApp.Models
-{
-    public class AddMovieDto
-    {
-        public required string Title { get; set; }
-        public required string Director { get; set; }
-        public required int Year { get; set; }
-    }
-}
--- MovieApp/Models/UpdateMovieDto.cs
+++
@@ -1,9 +0,0 @@
-namespace MovieApp.Models
-{
-    public class UpdateMovieDto
-    {
-        public required string Title { get; set; }
-        public required string Director { get; set; }
-        public required int Year { get; set; }
-    }
-}
